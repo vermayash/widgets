dd007bf93b7d453d72d17ef26ffb2376fa948a25
Yash Verma <yash.cverma@gmail.com>
2021-07-26 00:09:35 -0700

    added translate component

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
+import Convert from './Convert';
 import Dropdown from './Dropdown';
 
 const options = [
@@ -18,14 +19,32 @@ const options = [
 
 const Translate = () => {
     const [ language, setLanguage ] = useState(options[0]);
+    const [text, setText] = useState('');
 
     return (
         <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Text</label>
+                    <input 
+                        value={text} 
+                        onChange={
+                            (e) => setText(e.target.value)
+                         } 
+                    />
+                </div>
+            </div>
+            
             <Dropdown 
                 options = {options} 
                 selected={language}
                 onSelectedChange = {setLanguage}
                 label="Select a language" />
+
+            <hr />
+
+            <h3 className="ui header">Output</h3>
+            <Convert language={language} text={text} />
         </div>
     );
 };
